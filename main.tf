4fb84212a75d6745c7d4bee1b9d4ced96ccd75a0
DibyaJyotiNayak <121348451+Dibyaprivate@users.noreply.github.com>
2024-11-14 16:32:00 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-region = "ap-south-1"
+region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-  ami           = "ami-03753afda9b8ba740"
+  ami           = "ami-063d43db0594b521b"
   instance_type = "t2.micro"
   tags = {
     Name = "dev-server"
